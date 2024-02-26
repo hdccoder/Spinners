f1faaa5355f45a2c2d79a80fcb9d453d371bdbdc
Heather Lockett <heatherlockett@Heathers-MacBook-Pro.local>
2024-02-26 15:39:37 -0500

    Playlist page swiper logic added

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -1,34 +1,50 @@
 import React from 'react';
-import { useSearchParams, useParams } from 'react-router-dom';
-import { Typography, Grid } from '@mui/material';
+import { useSearchParams } from 'react-router-dom';
+import { Typography, Box, Container } from '@mui/material';
 import ProductCard from './ProductCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Playlist = ({ genre, products, cartItems, createLineItem, updateLineItem, auth, updateProduct }) => {
-    const [searchParams, setSearchParams] = useSearchParams();
-  
-    console.log('Genre in Playlist:', genre);
-    console.log('Products in Playlist:', products);
-  
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  
-    const filteredProducts = products
-      .filter(
-        (product) =>
-          (!searchParams.get('search') || product.name.toLowerCase().includes(searchParams.get('search').toLowerCase())) &&
-          (!genre || product.genre === genre)
-      );
-  
-    return (
-      <div>
-        <Typography variant="h4" gutterBottom>
-          {genre} Playlist
-        </Typography>
-        {/* ... (search bar) */}
-        <Grid container spacing={2}>
-          {filteredProducts.map((product) => {
+  console.log('Genre in Playlist:', genre);
+  console.log('Products in Playlist:', products);
+
+  const filteredProducts = products.filter(
+    (product) =>
+      (!searchParams.get('search') || product.name.toLowerCase().includes(searchParams.get('search').toLowerCase())) &&
+      (!genre || product.genre === genre)
+  );
+
+  return (
+    <Box>
+      <Typography variant="h4" gutterBottom>
+        {genre} Playlist
+      </Typography>
+      {/* ... (search bar) */}
+      <Container maxWidth="xl" sx={{ height: "25rem" }}>
+        <Swiper
+          modules={[Navigation, Pagination, A11y, Autoplay]}
+          spaceBetween={5}
+          slidesPerView={4}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+        >
+          {filteredProducts.map((product, index) => {
             const cartItem = cartItems.find((lineItem) => lineItem.product_id === product.id);
             return (
-              <Grid item key={product.id}>
+              <SwiperSlide key={product.id}>
                 <ProductCard
                   product={product}
                   cartItem={cartItem}
@@ -38,12 +54,13 @@ const Playlist = ({ genre, products, cartItems, createLineItem, updateLineItem, 
                   updateProduct={updateProduct}
                 />
                 {/* ... (other card content) */}
-              </Grid>
+              </SwiperSlide>
             );
           })}
-        </Grid>
-      </div>
-    );
-  };
-  
-  export default Playlist;
+        </Swiper>
+      </Container>
+    </Box>
+  );
+};
+
+export default Playlist;
