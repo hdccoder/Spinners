dedf7ebb8b446262ef034dfe77edc5a9a5861dfd
Heather Lockett <heatherlockett@Heathers-MacBook-Pro.local>
2024-02-27 00:36:35 -0500

    Playlist framework cont'd

--- src/Components/GenrePage.js
+++ src/Components/GenrePage.js
@@ -5,28 +5,34 @@ import Playlist from './Playlist';
 
 const GenrePage = ({ products, cartItems, createLineItem, updateLineItem, auth, updateProduct }) => {
     const { genre } = useParams();
-    const genres = ['Soul', 'Pop', 'R&B', 'Rock'];
-  
+    const genres = [
+        { name: 'Soul', image: '/public/assets/Soul.jpg' },
+        { name: 'Pop', image: '/public/assets/Pop.jpg' },
+        { name: 'RnB', image: '/public/assets/RnB.jpg' },
+        { name: 'Rock', image: '/public/assets/Rock.jpg' },
+    ];
+
     console.log('Current Genre in GenrePage:', genre);
     console.log('Products in GenrePage:', products);
 
-  return (
-    <div>
-      {genres.map((genreItem) => (
-        <Box key={genreItem} mb={4}>
-          <Playlist
-            genre={genreItem}
-            products={products}
-            cartItems={cartItems}
-            createLineItem={createLineItem}
-            updateLineItem={updateLineItem}
-            auth={auth}
-            updateProduct={updateProduct}
-          />
-        </Box>
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            {genres.map((genreItem) => (
+                <Box key={genreItem.name} mb={4}>
+                    <Playlist
+                        genre={genreItem.name}
+                        genreImage={genreItem.image}
+                        products={products}
+                        cartItems={cartItems}
+                        createLineItem={createLineItem}
+                        updateLineItem={updateLineItem}
+                        auth={auth}
+                        updateProduct={updateProduct}
+                    />
+                </Box>
+            ))}
+        </div>
+    );
 };
 
 export default GenrePage;
--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -11,55 +11,67 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-const Playlist = ({ genre, products, cartItems, createLineItem, updateLineItem, auth, updateProduct }) => {
+
+const Playlist = ({ genre, genreImage, products, cartItems, createLineItem, updateLineItem, auth, updateProduct }) => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   console.log('Genre in Playlist:', genre);
   console.log('Products in Playlist:', products);
 
   const filteredProducts = products.filter(
-    (product) =>
-      (!searchParams.get('search') || product.name.toLowerCase().includes(searchParams.get('search').toLowerCase())) &&
-      (!genre || product.genre === genre)
+      (product) =>
+          (!searchParams.get('search') || product.name.toLowerCase().includes(searchParams.get('search').toLowerCase())) &&
+          (!genre || product.genre === genre)
   );
 
   return (
-    <Box>
-      <Typography variant="h4" gutterBottom>
-        {genre} Playlist
-      </Typography>
-      {/* ... (search bar) */}
-      <Container maxWidth="xl" sx={{ height: "25rem" }}>
-        <Swiper
-          modules={[Navigation, Pagination, A11y, Autoplay]}
-          spaceBetween={5}
-          slidesPerView={4}
-          navigation
-          pagination={{ clickable: true }}
-          autoplay={{
-            delay: 2000,
-            disableOnInteraction: false,
-          }}
-        >
-          {filteredProducts.map((product, index) => {
-            const cartItem = cartItems.find((lineItem) => lineItem.product_id === product.id);
-            return (
-              <SwiperSlide key={product.id}>
-                <ProductCard
-                  product={product}
-                  cartItem={cartItem}
-                  createLineItem={createLineItem}
-                  updateLineItem={updateLineItem}
-                  auth={auth}
-                  updateProduct={updateProduct}
-                />
-                {/* ... (other card content) */}
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </Container>
-    </Box>
+      <Box position="relative">
+          <Container
+              maxWidth="xl"
+              sx={{
+                  position: 'relative',
+                  height: '25rem',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+              }}
+          >
+              <img
+                  src={genreImage}
+                  alt={`${genre} Playlist`}
+                  style={{ maxWidth: '100%', height: '425', width: '662px', position: 'absolute', zIndex: 1, marginLeft:-42 }}
+              />
+              <Swiper
+                  modules={[Navigation, Pagination, A11y, Autoplay]}
+                  spaceBetween={5}
+                  slidesPerView={4}
+                  navigation
+                  pagination={{ clickable: true }}
+                  autoplay={{
+                      delay: 2000,
+                      disableOnInteraction: false,
+                  }}
+                  style={{ zIndex: 0 }}
+              >
+                  {filteredProducts.map((product, index) => {
+                      const cartItem = cartItems.find((lineItem) => lineItem.product_id === product.id);
+                      return (
+                          <SwiperSlide key={product.id}>
+                              <ProductCard
+                                  product={product}
+                                  cartItem={cartItem}
+                                  createLineItem={createLineItem}
+                                  updateLineItem={updateLineItem}
+                                  auth={auth}
+                                  updateProduct={updateProduct}
+                              />
+                              {/* ... (other card content) */}
+                          </SwiperSlide>
+                      );
+                  })}
+              </Swiper>
+          </Container>
+      </Box>
   );
 };
 
