a9a5453c05613a4d26c02c364f5b1417d23e6a46
Heather Lockett <heatherlockett@Heathers-MacBook-Pro.local>
2024-03-13 01:19:30 -0400

    cont'd

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion, AccordionActions, AccordionDetails, AccordionSummary, Box, Container, Typography } from '@mui/material';
+import { Container, Typography, Box, Paper } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Link } from "react-router-dom";
 import Orders from './Components/Orders';
@@ -21,84 +21,46 @@ const UserProfile = ({
     updateLineItem,
     isProductInWishlist}) => {
 
-
-
     return (
     <Container>
-      <Typography variant="h3" align="center" sx={{ m: 4 }} >
-        Welcome {user.firstname} {user.lastname}
-      </Typography>
-      <Accordion  elevation={5} sx={{ mt: 0, mb: 4}} >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1-content"
-          id="panel1-header"
-          sx={{backgroundColor: 'primary.light'}}
-          elevation={5} 
-        >
+        <Typography variant="h3" align="center" sx={{ m: 4 }} >
+            Welcome {user.firstname} {user.lastname}
+        </Typography>
+
+        {/* Profile Section */}
+        <Paper elevation={3} sx={{ mt: 4, p: 2 }}>
             <Box>
-                <Typography variant='h4'>
-                    Profile
-                </Typography>
-                <Typography variant='h6'>
-                    View or update your personal information
-                </Typography>
+                <Typography variant='h4'>Profile</Typography>
+                <Typography variant='h6'>View or update your personal information</Typography>
             </Box>
-        </AccordionSummary>
-        <AccordionDetails>
-          <ProfileSettings user={user} setUser={setUser} />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion elevation={5} sx={{ mt: 0, mb: 4}} >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2-content"
-          id="panel2-header"
-          sx={{backgroundColor: 'primary.light'}}
-          elevation={5} 
-        >
+            <ProfileSettings user={user} setUser={setUser} />
+     
+
+        {/* Orders Section */}
+        
             <Box>
-                <Typography variant='h4'>
-                    Orders
-                </Typography>
-                <Typography variant='h6'>
-                    Track recent orders or buy again
-                </Typography>
+                <Typography variant='h4'>Orders</Typography>
+                <Typography variant='h6'>Track recent orders or buy again</Typography>
             </Box>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Orders orders={orders} products={products} lineItems={lineItems} getCartItem={getCartItem} 
-          createLineItem={createLineItem} updateLineItem={updateLineItem} 
-          createWishlistItem={createWishlistItem}
-          deleteWishlistItem={deleteWishlistItem}
-          isProductInWishlist={isProductInWishlist}
-          />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion elevation={5} sx={{ mt: 0, mb: 4}} >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3-content"
-          id="panel3-header"
-          sx={{ backgroundColor: 'primary.light'}}
-          elevation={5} 
-        >
+            <Orders orders={orders} products={products} lineItems={lineItems} getCartItem={getCartItem} 
+            createLineItem={createLineItem} updateLineItem={updateLineItem} 
+            createWishlistItem={createWishlistItem}
+            deleteWishlistItem={deleteWishlistItem}
+            isProductInWishlist={isProductInWishlist}
+            />
+      
+
+        {/* Wishlist Section */}
+      
             <Box>
-                <Typography variant='h4'>
-                    Wishlist
-                </Typography>
-                <Typography variant='h6'>
-                    View and modify your wishlist
-                </Typography>
+                <Typography variant='h4'>Wishlist</Typography>
+                <Typography variant='h6'>View your wishlist</Typography>
             </Box>
-        </AccordionSummary>
-        <AccordionDetails>
             <Wishlist wishlistItems={wishlistItems} products={products} getCartItem={getCartItem} 
             cartItems={cartItems} createLineItem={createLineItem} updateLineItem={updateLineItem} 
             deleteWishlistItem={deleteWishlistItem}
             />
-        </AccordionDetails>
-      </Accordion>
+        </Paper>
     </Container>
   );
 }
